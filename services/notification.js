e5616dbe6bdb81a242a6501dad3cbde5cd92bf04
Rita Letieri <daniel.letieri@gmail.com>
2025-04-02 21:00:41 +0400

    fixing issues

--- services/notification.js
+++ services/notification.js
@@ -1,13 +1,36 @@
 const nodemailer = require('nodemailer');
+const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
+// const emailTransporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const emailTransporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true para SSL (porta 465)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
+// Configuração do Telegram
+console.log('Configurando Telegram com o seguinte token:');
+console.log('TELEGRAM_BOT_TOKEN:', process.env.TELEGRAM_BOT_TOKEN);
+const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+
+// Função para capturar chatId (para depuração)
+telegramBot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log(`ChatID do usuário ${msg.from.first_name}: ${chatId}`);
+});
+
 
 async function sendEmail(to, subject, text) {
   const mailOptions = { from: process.env.EMAIL_USER, to, subject, text };
@@ -19,4 +42,16 @@ async function sendEmail(to, subject, text) {
   }
 }
 
-module.exports = { sendEmail };
+// Função de envio de mensagem via Telegram
+async function sendTelegram(to, message) {
+  try {
+    await telegramBot.sendMessage(to, message);
+    return { success: true, message: `Telegram enviado para ${to}` };
+  } catch (error) {
+    return { success: false, message: `Erro ao enviar Telegram: ${error.message}` };
+  }
+}
+
+
+
+module.exports = { sendEmail, sendTelegram };
